0dcfb2f95b78d6d294d35b8981272f73213274a3
ja1423 <aziabdujabborova1423@gmail.com>
2024-05-08 08:03:05 +0500

    admin

--- src/admin/dto/create-admin.dto.ts
+++ src/admin/dto/create-admin.dto.ts
@@ -1,16 +1,36 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
 export class CreateAdminDto {
+  @ApiProperty({
+    example: "Admin",
+    description: "Admin full_name",
+  })
   @IsString()
   @IsNotEmpty()
   full_name: string;
 
+
+  @ApiProperty({
+    example: "Admin",
+    description: "Admin phone_number",
+  })
   @IsString()
   phone_number?: string;
 
+
+  @ApiProperty({
+    example: "Admin",
+    description: "Admin email",
+  })
   @IsEmail()
   email?: string;
 
+
+  @ApiProperty({
+    example: "Admin",
+    description: "Admin password",
+  })
   @IsString()
   password: string;
 
